aeff614b9aac896d4b8779697f68ba47d17adf0a
tak-ksugio <ksugio@taknet.co.jp>
2021-08-31 10:31:04 +0900

    テスト

---
+++ src/main/java/jp/co/taknet/sample/Sample.java
@@ -0,0 +1,26 @@
+package jp.co.taknet.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Sample extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO 自動生成されたメソッド・スタブ
+		PrintWriter out = resp.getWriter();
+		out.println("Hello GET");	
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO 自動生成されたメソッド・スタブ
+		PrintWriter out = resp.getWriter();
+		out.println("Hello PUT");	
+	}
+}
